8bf55a3a0f307f0c282c46b3e44dda3b22d16cbe
Brianna <bri@songoda.com>
2019-06-11 00:14:06 -0400

    Fixed conflict with other epic plugins.

--- src/main/java/com/songoda/epicanchors/listeners/BlockListeners.java
+++ src/main/java/com/songoda/epicanchors/listeners/BlockListeners.java
@@ -2,6 +2,8 @@ package com.songoda.epicanchors.listeners;
 
 import com.songoda.epicanchors.EpicAnchors;
 import com.songoda.epicanchors.anchor.Anchor;
+import com.songoda.epicanchors.utils.settings.Setting;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -23,6 +25,7 @@ public class BlockListeners implements Listener {
 
         if (!item.hasItemMeta()
                 || !item.getItemMeta().hasDisplayName()
+                || Material.valueOf(Setting.MATERIAL.getString()) != event.getBlock().getType()
                 || plugin.getTicksFromItem(item) == 0) return;
 
         Anchor anchor = new Anchor(event.getBlock().getLocation(), plugin.getTicksFromItem(item));
